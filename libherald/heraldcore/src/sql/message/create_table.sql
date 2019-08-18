cd036d9defd68acd753ea697b612d3860a915015
Baeo Maltinksy <baeo.maltinsky@gmail.com>
2019-08-18 12:33:09 -0700

    Fix datatypes in messages schema

--- libherald/heraldcore/src/sql/message/create_table.sql
+++ libherald/heraldcore/src/sql/message/create_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS messages (
   id INTEGER PRIMARY KEY,
-  author id NOT NULL,
-  recipient id NOT NULL,
+  author TEXT NOT NULL,
+  recipient TEXT NOT NULL,
   timestamp TEXT NOT NULL,
-  body TEXT NOT NULL,
+  body BLOB NOT NULL,
   FOREIGN KEY(author) REFERENCES contacts (id),
   FOREIGN KEY(recipient) REFERENCES contacts (id)
 )
