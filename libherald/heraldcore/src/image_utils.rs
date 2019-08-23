6a6c1d9f224e054d62bab2b68e7c047e32acc1be
Baeo Maltinksy <baeo.maltinsky@gmail.com>
2019-08-22 17:49:21 -0700

    Fix image saving

--- libherald/heraldcore/src/image_utils.rs
+++ libherald/heraldcore/src/image_utils.rs
@@ -4,15 +4,16 @@ use lazy_static::*;
 use std::path::{Path, PathBuf};
 
 const IMAGE_SIZE: u32 = 300;
+const PROFILE_PICTURE: &str = "profile_pictures";
 
 lazy_static! {
-    static ref IMAGE_PATH: PathBuf = PathBuf::from("profile_pictures");
+    static ref IMAGE_PATH: PathBuf = PathBuf::from(PROFILE_PICTURE);
 }
 
 /// Determines path of profile picture for user id.
 pub fn profile_picture_path(id: &str) -> PathBuf {
     let mut image_path = IMAGE_PATH.clone();
-    image_path.push(id);
+    image_path.push(format!("{}.png", id));
     image_path
 }
 
@@ -22,11 +23,13 @@ pub fn save_profile_picture<P>(id: &str, source: P) -> Result<PathBuf, HErr>
 where
     P: AsRef<Path>,
 {
+    std::fs::create_dir_all(PROFILE_PICTURE)?;
+
     let image_path = profile_picture_path(id);
+
     image::open(source)?
         .resize_exact(IMAGE_SIZE, IMAGE_SIZE, FilterType::Nearest)
         .save_with_format(&image_path, ImageFormat::PNG)?;
-
     Ok(image_path)
 }
 
@@ -34,4 +37,15 @@ pub fn delete_profile_picture(id: &str) -> Result<(), HErr> {
     let image_path = profile_picture_path(id);
 
     Ok(std::fs::remove_file(image_path)?)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn profile_picture_path() {
+        assert_eq!(
+            super::profile_picture_path("yoyo"),
+            super::PathBuf::from("profile_pictures/yoyo.png")
+        );
+    }
 }
