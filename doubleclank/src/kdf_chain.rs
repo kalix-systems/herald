9b95075f2b0b3c6c1ff18320cf54c0eb53009b8e
theseriousadult <jack@gallabytes.com>
2019-09-01 00:18:45 -0700

    switch back to old chainkey ratchet mechanism
    and make the API a bit nicer.
    
    main reason for this is that I realized it does have better
    security properties in the context of disappearing messages.
    
    always remember to delete your keys when you're done with them!

--- doubleclank/src/kdf_chain.rs
+++ doubleclank/src/kdf_chain.rs
@@ -12,30 +12,44 @@ impl From<kx::SessionKey> for ChainKey {
 }
 
 impl ChainKey {
-    fn kdf(&self, idx: u64) -> MessageKey {
-        let mut output = [0u8; MSG_KEY_BYTES];
-        crate::utils::kdf_derive(&(self.0).0, idx, 0, &mut output);
-        MessageKey(secretbox::Key(output))
+    fn kdf(&self) -> (ChainKey, MessageKey) {
+        let mut chain_out = [0u8; CHAIN_KEY_BYTES];
+        let mut msg_out = [0u8; MSG_KEY_BYTES];
+
+        crate::utils::kdf_derive(&(self.0).0, 0, 0, &mut chain_out);
+        crate::utils::kdf_derive(&(self.0).0, 1, 0, &mut msg_out);
+
+        let chain_key = ChainKey(secretbox::Key(chain_out));
+        let msg_key = MessageKey(secretbox::Key(msg_out));
+
+        (chain_key, msg_key)
     }
 }
 
 pub struct Chain {
-    msg_id: u64,
     key: ChainKey,
 }
 
 impl Chain {
     pub fn new(key: ChainKey) -> Self {
-        Chain { msg_id: 0, key }
-    }
-
-    fn kdf(&self) -> MessageKey {
-        self.key.kdf(self.msg_id)
+        Chain { key }
     }
 
     fn ratchet(&mut self) -> MessageKey {
-        let res = self.kdf();
-        self.msg_id += 1;
+        let (new, res) = self.key.kdf();
+        self.key = new;
+        res
+    }
+
+    pub fn with_key<F, X>(&mut self, f: F) -> Option<X>
+    where
+        F: FnOnce(MessageKey) -> Option<X>,
+    {
+        let (new, msg_key) = self.key.kdf();
+        let res = f(msg_key);
+        if res.is_some() {
+            self.key = new;
+        }
         res
     }
 
@@ -44,12 +58,7 @@ impl Chain {
     }
 
     pub fn open<'a>(&mut self, cipher: Ciphertext<'a>) -> Option<&'a mut [u8]> {
-        let msg_key = self.kdf();
-        let res = msg_key.open(cipher);
-        if res.is_some() {
-            self.msg_id += 1;
-        }
-        res
+        self.with_key(move |key| key.open(cipher))
     }
 }
 
@@ -66,6 +75,14 @@ mod tests {
         assert!(
             libsodium_sys::crypto_kdf_blake2b_BYTES_MIN as usize <= MSG_KEY_BYTES,
             "keygen will fail - msg keys too short"
+        );
+        assert!(
+            CHAIN_KEY_BYTES <= libsodium_sys::crypto_kdf_blake2b_BYTES_MAX as usize,
+            "keygen will fail - chain keys too long"
+        );
+        assert!(
+            libsodium_sys::crypto_kdf_blake2b_BYTES_MIN as usize <= CHAIN_KEY_BYTES,
+            "keygen will fail - chain keys too short"
         );
     }
 }
