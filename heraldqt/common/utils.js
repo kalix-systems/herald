d38fd387f400d9a5254ad703ad64d484374d3e21
Baeo Maltinksy <baeo.maltinsky@gmail.com>
2019-09-04 22:38:16 -0700

    Clean up timestamp pretty printer

--- heraldqt/common/utils.js
+++ heraldqt/common/utils.js
@@ -3,27 +3,33 @@
 function unwrap_or(maybe_val, fallback) {
   // TODO this produces false positives
   if (maybe_val) {
-   return maybe_val
+    return maybe_val;
   } else {
-    return fallback
+    return fallback;
   }
 }
 
-function friendly_timestamp(ms_epoch_time){
+function friendly_timestamp(ms_epoch_time) {
+  const second_ms_ratio = 1000;
+  const seconds_per_minute = 60;
+  const seconds_per_hour = 3600;
+  const seconds_per_day = 3600 * 24;
 
-    const dt = new Date(ms_epoch_time);
-    const now =  Date.now();
-    const diff = (now - dt) / 1000;
+  const dt = new Date(ms_epoch_time);
+  const now = Date.now();
+  const diff = (now - dt) / second_ms_ratio;
 
-    if(diff < 0) return ""
-    if(diff < 60) return "just now"
-    if(diff < 3600) return Math.floor(diff/60) + " minutes ago"
-    if(diff < 3600*24 ) return Math.floor(diff/(3600)) + " hours ago"
-    return dt.toDateString()
+  if (diff < 0) return "";
 
+  if (diff < seconds_per_minute) return "NOW";
 
+  if (diff < seconds_per_hour) {
+    return Math.floor(diff / seconds_per_minute) + " MIN AGO";
+  }
 
-    // check in minutes
-    // check in hours
-    // send date
+  if (diff < seconds_per_day) {
+    return Math.floor(diff / seconds_per_hour) + " HR AGO";
+  }
+
+  return dt.toDateString();
 }
