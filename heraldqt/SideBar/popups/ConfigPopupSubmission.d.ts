b7ab609679fd301425223ebd298b35d0329b937c
Baeo Maltinksy <baeo.maltinsky@gmail.com>
2019-09-14 15:31:23 -0700

    Update type declarations

--- heraldqt/SideBar/popups/ConfigPopupSubmission.d.ts
+++ heraldqt/SideBar/popups/ConfigPopupSubmission.d.ts
@@ -1,1 +1,1 @@
-export declare function submit(config: Config, useridField: TextField, usernameField: TextField): void;
+export declare function submit(config: Config, configInit: boolean, useridField: TextField, usernameField: TextField): void;
