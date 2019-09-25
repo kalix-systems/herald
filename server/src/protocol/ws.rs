3bbc9d661a414944e303a81253d8a7701e27a540
theseriousadult <jack@kalix.io>
2019-09-25 01:20:46 -0700

    remove unnecessary self parameters from login helpers

--- server/src/protocol/ws.rs
+++ server/src/protocol/ws.rs
@@ -51,79 +51,69 @@ impl State {
             self.active.insert(g.did, sender);
             // TODO: handle this error somehow?
             // for now we're just dropping it
-            if self.catchup(g.did, &mut store, &mut ws).await.is_ok() {
+            if catchup(g.did, &mut store, &mut ws).await.is_ok() {
                 // TODO: maybe handle this one too?
                 // again just dropping it since the flow must go on
-                drop(self.send_pushes(ws, &mut receiver).await);
+                drop(send_pushes(ws, &mut receiver).await);
             }
-            self.active.remove(&g.did);
-            self.archive_pushes(&mut store, receiver, g.did).await?;
+            active.remove(&g.did);
+            archive_pushes(&mut store, receiver, g.did).await?;
         }
 
         Ok(())
     }
+}
 
-    async fn catchup<S, W, E>(
-        &self,
-        did: sign::PublicKey,
-        s: &mut S,
-        ws: &mut W,
-    ) -> Result<(), Error>
-    where
-        S: Store,
-        W: Stream<Item = Result<ws::Message, warp::Error>> + Sink<ws::Message, Error = E> + Unpin,
-        Error: From<E>,
-    {
-        use catchup::*;
-        let pending = s.get_pending(did)?;
-
-        // TCP over TCP...
-        for chunk in pending.chunks(CHUNK_SIZE) {
-            // TODO: remove unnecessary memcpy here by using a draining chunk iterator?
-            let msg = Catchup(Vec::from(chunk));
-            loop {
-                ws.send(ws::Message::binary(serde_cbor::to_vec(&msg)?))
-                    .await?;
-
-                let m = ws.next().await.ok_or(CatchupFailed)??;
-
-                if CatchupAck(chunk.len() as u64) == serde_cbor::from_slice(m.as_bytes())? {
-                    break;
-                }
-            }
-        }
-
-        s.expire_pending(did)?;
-
-        Ok(())
+async fn send_pushes<Tx, E, Rx>(mut tx: Tx, rx: &mut Rx) -> Result<(), Error>
+where
+    Tx: Sink<ws::Message, Error = E> + Unpin,
+    Error: From<E>,
+    Rx: Stream<Item = Push> + Unpin,
+{
+    while let Some(p) = rx.next().await {
+        tx.send(ws::Message::binary(serde_cbor::to_vec(&p)?))
+            .await?;
     }
+    Ok(())
+}
 
-    async fn send_pushes<Tx, E, Rx>(&self, mut tx: Tx, rx: &mut Rx) -> Result<(), Error>
-    where
-        Tx: Sink<ws::Message, Error = E> + Unpin,
-        Error: From<E>,
-        Rx: Stream<Item = Push> + Unpin,
-    {
-        while let Some(p) = rx.next().await {
-            tx.send(ws::Message::binary(serde_cbor::to_vec(&p)?))
+async fn archive_pushes<S, Rx>(store: &mut S, mut rx: Rx, to: sig::PublicKey) -> Result<(), Error>
+where
+    S: Store,
+    Rx: Stream<Item = Push> + Unpin,
+{
+    while let Some(p) = rx.next().await {
+        store.add_pending(vec![to], p)?;
+    }
+    Ok(())
+}
+
+async fn catchup<S, W, E>(did: sign::PublicKey, s: &mut S, ws: &mut W) -> Result<(), Error>
+where
+    S: Store,
+    W: Stream<Item = Result<ws::Message, warp::Error>> + Sink<ws::Message, Error = E> + Unpin,
+    Error: From<E>,
+{
+    use catchup::*;
+    let pending = s.get_pending(did)?;
+
+    // TCP over TCP...
+    for chunk in pending.chunks(CHUNK_SIZE) {
+        // TODO: remove unnecessary memcpy here by using a draining chunk iterator?
+        let msg = Catchup(Vec::from(chunk));
+        loop {
+            ws.send(ws::Message::binary(serde_cbor::to_vec(&msg)?))
                 .await?;
+
+            let m = ws.next().await.ok_or(CatchupFailed)??;
+
+            if CatchupAck(chunk.len() as u64) == serde_cbor::from_slice(m.as_bytes())? {
+                break;
+            }
         }
-        Ok(())
     }
 
-    async fn archive_pushes<S, Rx>(
-        &self,
-        store: &mut S,
-        mut rx: Rx,
-        to: sig::PublicKey,
-    ) -> Result<(), Error>
-    where
-        S: Store,
-        Rx: Stream<Item = Push> + Unpin,
-    {
-        while let Some(p) = rx.next().await {
-            store.add_pending(vec![to], p)?;
-        }
-        Ok(())
-    }
+    s.expire_pending(did)?;
+
+    Ok(())
 }
