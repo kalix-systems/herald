34bd3f92b3998ae656557a0a7261db6ee42820a6
Baeo Maltinksy <baeo.maltinsky@gmail.com>
2019-09-27 22:44:29 -0700

    Improve chainkeys test

--- libherald/heraldcore/src/chainkeys/sql/get_unused.sql
+++ libherald/heraldcore/src/chainkeys/sql/get_unused.sql
@@ -4,4 +4,4 @@ SELECT
 FROM
   chainkeys
 WHERE
-  (conversation_id = @1) AND (used = 0)
+  ((conversation_id = @1) AND (used = 0))
--- libherald/heraldcore/src/chainkeys/tests.rs
+++ libherald/heraldcore/src/chainkeys/tests.rs
@@ -7,39 +7,82 @@ use serial_test_derive::serial;
 #[serial]
 fn blockstore() {
     Database::reset_all().expect(womp!());
-    let conversation_id = ConversationId::from(crate::utils::rand_id());
-    add_conversation(Some(&conversation_id), None).expect(womp!());
-    let mut handle = ChainKeys::new(conversation_id);
+    let cid1 = ConversationId::from(crate::utils::rand_id());
+    let cid2 = ConversationId::from(crate::utils::rand_id());
 
-    let blockhash1 = BlockHash::from_slice(vec![1; BLOCKHASH_BYTES].as_slice()).expect(womp!());
-    let blockhash2 = BlockHash::from_slice(vec![2; BLOCKHASH_BYTES].as_slice()).expect(womp!());
-    let chainkey1 = ChainKey::from_slice(vec![1; CHAINKEY_BYTES].as_slice()).expect(womp!());
-    let chainkey2 = ChainKey::from_slice(vec![2; CHAINKEY_BYTES].as_slice()).expect(womp!());
+    add_conversation(Some(&cid1), None).expect(womp!());
+    add_conversation(Some(&cid2), None).expect(womp!());
 
-    handle
-        .store_key(blockhash1, (&chainkey1).clone())
+    let mut handle1 = ChainKeys::new(cid1);
+    let mut handle2 = ChainKeys::new(cid2);
+
+    let blockhash11 = BlockHash::from_slice(vec![11; BLOCKHASH_BYTES].as_slice()).expect(womp!());
+    let blockhash12 = BlockHash::from_slice(vec![12; BLOCKHASH_BYTES].as_slice()).expect(womp!());
+    let chainkey11 = ChainKey::from_slice(vec![11; CHAINKEY_BYTES].as_slice()).expect(womp!());
+    let chainkey12 = ChainKey::from_slice(vec![12; CHAINKEY_BYTES].as_slice()).expect(womp!());
+
+    let blockhash21 = BlockHash::from_slice(vec![21; BLOCKHASH_BYTES].as_slice()).expect(womp!());
+    let blockhash22 = BlockHash::from_slice(vec![22; BLOCKHASH_BYTES].as_slice()).expect(womp!());
+    let chainkey21 = ChainKey::from_slice(vec![21; CHAINKEY_BYTES].as_slice()).expect(womp!());
+    let chainkey22 = ChainKey::from_slice(vec![22; CHAINKEY_BYTES].as_slice()).expect(womp!());
+
+    // handle1 keys
+    handle1
+        .store_key(blockhash11, (&chainkey11).clone())
         .expect(womp!());
-    handle
-        .store_key(blockhash2, (&chainkey2).clone())
+    handle1
+        .store_key(blockhash12, (&chainkey12).clone())
         .expect(womp!());
 
-    let known_keys: BTreeSet<ChainKey> = vec![(&chainkey1).clone(), (&chainkey2).clone()]
+    // handle2 keys
+    handle2
+        .store_key(blockhash21, (&chainkey21).clone())
+        .expect(womp!());
+    handle2
+        .store_key(blockhash22, (&chainkey22).clone())
+        .expect(womp!());
+
+    // handle1 known keys
+    let known_keys1: BTreeSet<ChainKey> = vec![(&chainkey11).clone(), (&chainkey12).clone()]
         .into_iter()
         .collect();
 
-    let keys = handle
-        .get_keys(vec![blockhash1, blockhash2].iter())
+    let keys1 = handle1
+        .get_keys(vec![blockhash11, blockhash12].iter())
         .expect(womp!());
 
-    assert_eq!(keys.len(), 2);
-    assert_eq!(known_keys, keys);
+    assert_eq!(keys1.len(), 2);
+    assert_eq!(known_keys1, keys1);
 
-    handle.mark_used(vec![blockhash1].iter()).expect(womp!());
+    // handle2 known keys
+    let known_keys2: BTreeSet<ChainKey> = vec![(&chainkey21).clone(), (&chainkey22).clone()]
+        .into_iter()
+        .collect();
 
-    let unused: Vec<_> = handle.get_unused().expect(womp!()).into_iter().collect();
+    let keys2 = handle2
+        .get_keys(vec![blockhash21, blockhash22].iter())
+        .expect(womp!());
 
-    assert_eq!(unused.len(), 1);
-    assert_eq!(unused, vec![(blockhash2, chainkey2)]);
+    assert_eq!(keys2.len(), 2);
+    assert_eq!(known_keys2, keys2);
 
-    assert!(handle.mark_used(vec![blockhash1].iter()).is_err());
+    // handle1 mark used
+    handle1.mark_used(vec![blockhash11].iter()).expect(womp!());
+
+    let unused1: Vec<_> = handle1.get_unused().expect(womp!()).into_iter().collect();
+
+    assert_eq!(unused1.len(), 1);
+    assert_eq!(unused1, vec![(blockhash12, chainkey12)]);
+
+    assert!(handle1.mark_used(vec![blockhash11].iter()).is_err());
+
+    // handle2 mark used
+    handle2.mark_used(vec![blockhash21].iter()).expect(womp!());
+
+    let unused2: Vec<_> = handle2.get_unused().expect(womp!()).into_iter().collect();
+
+    assert_eq!(unused2.len(), 1);
+    assert_eq!(unused2, vec![(blockhash22, chainkey22)]);
+
+    assert!(handle1.mark_used(vec![blockhash21].iter()).is_err());
 }
