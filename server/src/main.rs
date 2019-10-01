6455b91ca6727f9f034ca0500d5b6978bc8c5947
theseriousadult <jack@kalix.io>
2019-09-30 17:58:19 -0700

    make main start docker with docker-compose instead

--- server/src/main.rs
+++ server/src/main.rs
@@ -15,44 +15,28 @@ async fn main() {
     ctrlc::set_handler(|| {
         println!("");
         println!("quitting");
-        Command::new("docker")
-            .args(&["kill", "pg-docker"])
+        Command::new("docker-compose")
+            .arg("down")
             .status()
-            .expect("failed to kill postgres");
+            .expect("failed to kill docker");
         std::process::exit(0)
     })
     .expect("failed to setup ctrlc handler");
 
     println!("starting docker container");
-    Command::new("docker")
-        .args(&["start", "pg-docker"])
+    Command::new("docker-compose")
+        .args(&["up", "-d"])
         .output()
         .expect("failed to start docker container");
-
-    // println!("fetching docker image");
-    // // Command::new("docker")
-    //     .args(&["pull", "postgres"])
-    //     .output()
-    //     .expect("failed to pull postgres image");
-    // Command::new("docker")
-    //     .arg("run")
-    //     .arg("--rm")
-    //     .args(&["--name", "pg-docker"])
-    //     .args(&["-e", "POSTGRESS_PASSWORD=docker"])
-    //     .args(&["-d", "-p", "5432:5432"])
-    //     .args(&["-v", PGVOLUME])
-    //     .arg("postgres")
-    //     .output()
-    //     .expect("failed to start postgres");
 
     println!("starting server");
     HANDLER.serve(8080).await;
 
     println!("server stopped - shutting down postgres");
-    Command::new("docker")
-        .args(&["kill", "pg-docker"])
-        .output()
-        .expect("failed to kill postgres");
+    Command::new("docker-compose")
+        .arg("down")
+        .status()
+        .expect("failed to kill docker");
 
     println!("all done");
 }
