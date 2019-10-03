46ff372a3b86682b49b6e14e184f75aca4234f5e
Baeo Maltinksy <baeo.maltinsky@gmail.com>
2019-10-02 18:13:38 -0700

    Add error queue

--- libherald/src/shared.rs
+++ libherald/src/shared.rs
@@ -2,6 +2,8 @@ use crossbeam_channel::*;
 use dashmap::DashMap;
 use heraldcore::types::{ConversationId, MsgId};
 use lazy_static::*;
+use parking_lot::Mutex;
+use std::collections::VecDeque;
 
 /// Conversation updates
 pub enum ConvUpdate {
@@ -11,8 +13,31 @@ pub enum ConvUpdate {
     Ack(MsgId),
 }
 
+type QueueError = (u8, String);
+
+/// Error queue
+#[derive(Default)]
+pub struct ErrorQueue(Mutex<VecDeque<QueueError>>);
+
+impl ErrorQueue {
+    /// Reads the oldest unread error.
+    pub fn read(&self) -> Option<QueueError> {
+        self.0.lock().pop_front()
+    }
+
+    /// Adds a new error to the back of the queue.
+    pub fn push(&self, e: QueueError) {
+        self.0.lock().push_back(e)
+    }
+}
+
 lazy_static! {
     /// Concurrent hash map from [`ConversationId`]s to an event stream.
     /// This is used to route notifications that arrive from the network.
     pub static ref CONV_MSG_RXS: DashMap<ConversationId, Receiver<ConvUpdate>> = DashMap::default();
+}
+
+lazy_static! {
+    /// Global error queue
+    pub static ref ERROR_QUEUE: ErrorQueue = ErrorQueue::default();
 }
