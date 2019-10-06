dde1dbceea5b0867fbbe5e2464130f20174fbeae
theseriousadult <jack@kalix.io>
2019-10-06 03:50:42 -0700

    remove ctrlc behavior from herald_server

--- herald_server/src/main.rs
+++ herald_server/src/main.rs
@@ -8,21 +8,8 @@ lazy_static! {
     static ref HANDLER: State = State::new();
 }
 
-// const PGVOLUME: &'static str = "/tmp/postgres_volume:/var/lib/postgresql/data";
-
 #[tokio::main]
 async fn main() {
-    ctrlc::set_handler(|| {
-        println!("");
-        println!("quitting");
-        Command::new("docker-compose")
-            .arg("down")
-            .status()
-            .expect("failed to kill docker");
-        std::process::exit(0)
-    })
-    .expect("failed to setup ctrlc handler");
-
     println!("starting docker container");
     Command::new("docker-compose")
         .args(&["up", "-d"])
@@ -31,12 +18,4 @@ async fn main() {
 
     println!("starting server");
     HANDLER.serve(8080).await;
-
-    println!("server stopped - shutting down postgres");
-    Command::new("docker-compose")
-        .arg("down")
-        .status()
-        .expect("failed to kill docker");
-
-    println!("all done");
 }
