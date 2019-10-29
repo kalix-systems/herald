d08a740a17a01dfd60cd1113b49f8718d0f110f2
Baeo Maltinksy <baeo.maltinsky@gmail.com>
2019-10-28 20:05:57 -0700

    Add scaffolding for kson redux

---
+++ kson/src/errors.rs
@@ -0,0 +1,76 @@
+use bytes::Bytes;
+use std::fmt;
+
+#[derive(Debug, Clone)]
+pub struct Error {
+    pub location: Location,
+    pub message: String,
+    pub bytes: Bytes,
+    pub variant: Variant,
+}
+
+#[derive(Debug, Clone)]
+pub enum Variant {}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.variant {}
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+/// A location in source code
+pub struct Location {
+    /// The line where the error occurred
+    pub line: u32,
+    /// The column where the error occurred
+    pub col: u32,
+    /// The file where the error occurred
+    pub file: &'static str,
+}
+
+impl fmt::Display for Location {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{file}:{line}:{column}",
+            file = self.file,
+            line = self.line,
+            column = self.file
+        )
+    }
+}
+
+#[macro_export]
+/// Returns the location this macro was called from
+macro_rules! loc {
+    () => {
+        $crate::errors::Location {
+            file: file!(),
+            line: line!(),
+            col: column!(),
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! E {
+    ($var: expr, $byt: expr, $($t: tt),*) => {
+        use $crate::loc;
+        use $crate::errors::Variant::*;
+
+        $crate::errors::Error {
+            location: loc!(),
+            bytes: $byt,
+            variant: $var,
+            message: format!($($t),*)
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! e {
+    ($var: expr, $byt: expr, $($t:tt),*) => {
+        Err::<(), $crate::errors::Error>(E!($var, $byt, $($t),*))?
+    }
+}
