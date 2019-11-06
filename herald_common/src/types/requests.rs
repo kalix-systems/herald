ba313bdd51bca8e21db3f3e9f7368ac595e4d5e6
theseriousadult <jack@kalix.io>
2019-11-05 22:02:48 -0800

    add tarpc types and traits to herald common

--- herald_common/src/types/requests.rs
+++ herald_common/src/types/requests.rs
@@ -1,5 +1,11 @@
 use super::*;
 
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub enum RequestError {
+    UnknownError,
+    DbError(String),
+}
+
 pub mod keys_of {
     use super::*;
 
@@ -133,4 +139,23 @@ pub mod get_prekeys {
         Success(Vec<(sig::PublicKey, sealed::PublicKey)>),
         Missing(Vec<sig::PublicKey>),
     }
+}
+
+#[tarpc::service]
+trait HeraldService {
+    async fn keys_of(req: keys_of::Req) -> Result<keys_of::Res, RequestError>;
+    async fn key_info(req: key_info::Req) -> Result<key_info::Res, RequestError>;
+    async fn keys_exist(req: keys_exist::Req) -> Result<keys_exist::Res, RequestError>;
+    async fn users_exist(req: users_exist::Req) -> Result<users_exist::Res, RequestError>;
+    async fn push_users(req: push_users::Req) -> Result<push_users::Res, RequestError>;
+    async fn push_devices(req: push_devices::Req) -> Result<push_devices::Res, RequestError>;
+    async fn new_key(req: new_key::Req) -> Result<new_key::Res, RequestError>;
+    async fn dep_key(req: dep_key::Req) -> Result<dep_key::Res, RequestError>;
+    async fn register(req: register::Req) -> Result<register::Res, RequestError>;
+    async fn add_prekeys(req: add_prekeys::Req) -> Result<add_prekeys::Res, RequestError>;
+    async fn get_prekeys(req: get_prekeys::Req) -> Result<get_prekeys::Res, RequestError>;
+}
+
+pub mod transport {
+    pub use tarpc_bincode_transport::*;
 }
