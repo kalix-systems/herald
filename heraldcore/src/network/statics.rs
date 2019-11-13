35af0792800acffccb790e2625a614628c22bd50
theseriousadult <jack@kalix.io>
2019-11-13 15:35:12 -0800

    initialize a global RPC client

--- heraldcore/src/network/statics.rs
+++ heraldcore/src/network/statics.rs
@@ -1,8 +1,9 @@
 use super::*;
 use lazy_static::*;
+use once_cell::sync::*;
 use std::{
     net::{Ipv4Addr, SocketAddr},
-    sync::atomic::AtomicBool,
+    sync::atomic::{AtomicBool, Ordering},
 };
 
 pub(super) const DEFAULT_SERVER_IP_ADDR: [u8; 4] = [127, 0, 0, 1];
@@ -21,9 +22,32 @@ lazy_static! {
 
 pub(super) static CAUGHT_UP: AtomicBool = AtomicBool::new(false);
 
+pub(super) static RPC_CLIENT: OnceCell<HeraldServiceClient> = OnceCell::new();
+static RPC_IS_BEING_INIT: AtomicBool = AtomicBool::new(false);
+
 /// Attempts to load the cached client, and creates a new one if one doesn't already exist.
 pub(super) async fn get_client() -> Result<HeraldServiceClient, HErr> {
-    let transport = transport::connect(&SERVER_RPC_ADDR).await?;
-    let client = HeraldServiceClient::new(tarpc::client::Config::default(), transport).spawn()?;
-    Ok(client)
+    if let Some(r) = RPC_CLIENT.get() {
+        Ok(r.clone())
+    } else {
+        if !RPC_IS_BEING_INIT.swap(true, Ordering::SeqCst) {
+            let transport = transport::connect(&SERVER_RPC_ADDR).await?;
+            let client =
+                HeraldServiceClient::new(tarpc::client::Config::default(), transport).spawn()?;
+            RPC_CLIENT
+                .set(client.clone())
+                .expect("failed to set client");
+            Ok(client)
+        } else {
+            // spin until it's ready
+            // 10 seconds really should be long enough
+            for _ in 0..100 {
+                tokio::timer::delay_for(std::time::Duration::from_millis(100)).await;
+                if let Some(r) = RPC_CLIENT.get() {
+                    return Ok(r.clone());
+                }
+            }
+            Err(HeraldError("failed to init rpc client".into()))
+        }
+    }
 }
