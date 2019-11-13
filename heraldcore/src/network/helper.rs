7db034e93ceda6a036f02338d003d7cd6398ae1c
Baeo Maltinsky <baeo.maltinsky@gmail.com>
2019-11-13 16:39:51 -0500

    Move helper module in different file

---
+++ heraldcore/src/network/helper.rs
@@ -0,0 +1,22 @@
+use crate::errors::*;
+use herald_common::*;
+
+macro_rules! mk_request {
+        ($path: tt) => {
+            pub async fn $path(req: $path::Req) -> Result<$path::Res, HErr> {
+                let mut client = super::get_client().await?;
+                let res = client.$path(tarpc::context::current(), req).await??;
+                Ok(res)
+            }
+        };
+    }
+
+mk_request!(keys_of);
+mk_request!(key_info);
+mk_request!(keys_exist);
+mk_request!(users_exist);
+mk_request!(register);
+mk_request!(new_key);
+mk_request!(dep_key);
+mk_request!(push_users);
+mk_request!(push_devices);
--- heraldcore/src/network/mod.rs
+++ heraldcore/src/network/mod.rs
@@ -19,30 +19,7 @@ use statics::*;
 mod types;
 pub use types::*;
 
-mod helper {
-    use crate::errors::*;
-    use herald_common::*;
-
-    macro_rules! mk_request {
-        ($path: tt) => {
-            pub async fn $path(req: $path::Req) -> Result<$path::Res, HErr> {
-                let mut client = super::get_client().await?;
-                let res = client.$path(tarpc::context::current(), req).await??;
-                Ok(res)
-            }
-        };
-    }
-
-    mk_request!(keys_of);
-    mk_request!(key_info);
-    mk_request!(keys_exist);
-    mk_request!(users_exist);
-    mk_request!(register);
-    mk_request!(new_key);
-    mk_request!(dep_key);
-    mk_request!(push_users);
-    mk_request!(push_devices);
-}
+mod helper;
 
 macro_rules! get_of_helper {
     ($name: tt, $of: ty, $to: ty) => {
