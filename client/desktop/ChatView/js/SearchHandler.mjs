c983549ee8e19c6758781d48e0a6a6d910f43f0b
Kaavya <kaavya.nj@gmail.com>
2019-11-15 18:54:57 -0800

    ts files

---
+++ client/desktop/ChatView/js/SearchHandler.mjs
@@ -0,0 +1,48 @@
+export function isOnscreen(ownedConversation, chatListView, chatPane, forward) {
+    if (!forward) {
+        const x = chatListView.itemAt(ownedConversation.peekPrevSearchMatch()).x;
+        const y = chatListView.itemAt(ownedConversation.peekPrevSearchMatch()).y;
+        const yPos = chatPane.mapFromItem(chatListView, x, y).y;
+        const pageHeight = chatPane.height - 50;
+        if (0 < yPos && yPos < pageHeight) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        const x = chatListView.itemAt(ownedConversation.peekNextSearchMatch()).x;
+        const y = chatListView.itemAt(ownedConversation.peekNextSearchMatch()).y;
+        const yPos = chatPane.mapFromItem(chatListView, x, y).y;
+        const pageHeight = chatPane.height - 50;
+        if (0 < yPos && yPos < pageHeight) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+export function jumpHandler(ownedConversation, chatListView, chatPane, conversationWindow, forward) {
+    const toJump = !isOnscreen(ownedConversation, chatListView, chatPane, forward);
+    const convoMiddle = conversationWindow.height / 2;
+    if (forward) {
+        if (toJump) {
+            conversationWindow.contentY = chatListView.itemAt(ownedConversation.nextSearchMatch()).y - convoMiddle;
+        }
+        else {
+            ownedConversation.nextSearchMatch();
+        }
+        return;
+    }
+    else {
+        if (toJump) {
+            conversationWindow.contentY = chatListView.itemAt(ownedConversation.prevSearchMatch()).y - convoMiddle;
+        }
+        else {
+            ownedConversation.prevSearchMatch();
+        }
+        return;
+    }
+}
