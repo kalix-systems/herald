c2e1e8371e10eeaf60d7e9612170e8687b624922
Kaavya <kaavya.nj@gmail.com>
2019-11-22 12:44:41 -0800

    fixed tests ?

--- client/tests/foundation/main.cpp
+++ client/tests/foundation/main.cpp
@@ -9,18 +9,18 @@ public:
   Setup()
   {
     qmlRegisterType<Users>("LibHerald", 1, 0, "Users");
-    qmlRegisterType<Members>("LibHerald", 1, 0, "Members");
-    qmlRegisterType<Messages>("LibHerald", 1, 0, "Messages");
-    qmlRegisterType<Conversations>("LibHerald", 1, 0, "Conversations");
-    qmlRegisterType<Config>("LibHerald", 1, 0, "Config");
-    qmlRegisterType<HeraldState>("LibHerald", 1, 0, "HeraldState");
-    qmlRegisterType<HeraldUtils>("LibHerald", 1, 0, "HeraldUtils");
-    qmlRegisterType<Errors>("LibHerald", 1, 0, "Errors");
-    qmlRegisterType<ConversationBuilder>("LibHerald", 1, 0,
-                                         "ConversationBuilder");
-    qmlRegisterType<MessageBuilder>("LibHerald", 1, 0, "MessageBuilder");
-    qmlRegisterType<Attachments>("LibHerald", 1, 0, "Attachments");
-    qmlRegisterType<MessagePreview>("LibHerald", 1, 0, "MessagePreview");
+	qmlRegisterType<Members>("LibHerald", 1, 0, "Members");
+	qmlRegisterType<Messages>("LibHerald", 1, 0, "Messages");
+	qmlRegisterType<Conversations>("LibHerald", 1, 0, "Conversations");
+	qmlRegisterType<Config>("LibHerald", 1, 0, "Config");
+	qmlRegisterType<Herald>("LibHerald", 1, 0, "Herald");
+	qmlRegisterType<Utils>("LibHerald", 1, 0, "Utils");
+	qmlRegisterType<Errors>("LibHerald", 1, 0, "Errors");
+	qmlRegisterType<ConversationBuilder>("LibHerald", 1, 0, "ConversationBuilder");
+	qmlRegisterType<MessageBuilder>("LibHerald", 1, 0, "MessageBuilder");
+	qmlRegisterType<Attachments>("LibHerald", 1, 0, "Attachments");
+	qmlRegisterType<UsersSearch>("LibHerald", 1, 0, "UsersSearch");
+	qmlRegisterType<MessageSearch>("LibHerald", 1, 0, "MessageSearch");
     qmlRegisterSingletonType(QUrl("qrc:///common/CommonConfig.qml"),
                              "LibHerald", 1, 0, "CmnCfg");
   }
