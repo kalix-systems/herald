98d6059a6d38ff0e255bfa56fb2b1c798cb65d7d
theseriousadult <jack@kalix.io>
2019-11-22 00:32:59 -0800

    replace rand_id with MsgId::new

--- heraldcore/src/message/db.rs
+++ heraldcore/src/message/db.rs
@@ -258,7 +258,7 @@ impl OutboundMessageBuilder {
         }
 
         let conversation_id = e!(conversation.ok_or(MissingConversationId));
-        let msg_id: MsgId = utils::rand_id().into();
+        let msg_id = MsgId::new();
         let timestamp = Time::now();
         let author = e!(crate::config::db::id(&db));
         let expiration_period = e!(expiration_period(&db, &conversation_id));
