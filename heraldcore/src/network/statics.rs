1a650973d42e14cbec5abf9f0dfb129bca4efaaf
Baeo Maltinsky <baeo.maltinsky@gmail.com>
2019-12-03 17:55:28 -0500

    Fix complaint complaints and e2e test

--- heraldcore/src/network/statics.rs
+++ heraldcore/src/network/statics.rs
@@ -8,15 +8,14 @@ static HOME_SERVER: OnceCell<SocketAddr> = OnceCell::new();
 static DEFAULT_SERVER: OnceCell<SocketAddr> = OnceCell::new();
 
 pub(super) fn home_server() -> &'static SocketAddr {
-    match HOME_SERVER.get_or_try_init(|| crate::config::home_server()) {
+    match HOME_SERVER.get_or_try_init(crate::config::home_server) {
         Ok(addr) => addr,
         Err(_) => default_server(),
     }
 }
 
 pub(crate) fn default_server() -> &'static SocketAddr {
-    DEFAULT_SERVER
-        .get_or_init(|| SocketAddr::new(DEFAULT_SERVER_IP_ADDR.into(), DEFAULT_PORT.into()))
+    DEFAULT_SERVER.get_or_init(|| SocketAddr::new(DEFAULT_SERVER_IP_ADDR.into(), DEFAULT_PORT))
 }
 
 pub(super) static CAUGHT_UP: AtomicBool = AtomicBool::new(false);
