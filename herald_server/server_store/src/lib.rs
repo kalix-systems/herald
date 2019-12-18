de4d54b600eee5a048fc410609249375e5167d6e
Baeo Maltinsky <baeo.maltinsky@gmail.com>
2019-12-17 19:41:39 -0800

    Add send to trait bounds

--- herald_server/server_store/src/lib.rs
+++ herald_server/server_store/src/lib.rs
@@ -34,23 +34,23 @@ pub trait ServerStore {
         key: sig::PublicKey,
     ) -> Result<Option<UserId>, Error>;
 
-    async fn new_prekeys<Keys: Stream<Item = (Signed<Prekey>, Option<Prekey>)>>(
+    async fn new_prekeys<Keys: Stream<Item = (Signed<Prekey>, Option<Prekey>)> + Send>(
         &mut self,
         keys: Keys,
     ) -> Result<new_prekeys::Res, Error>;
 
-    async fn get_random_prekeys<Keys: Stream<Item = sig::PublicKey>>(
+    async fn get_random_prekeys<Keys: Stream<Item = sig::PublicKey> + Send>(
         &mut self,
         keys: Keys,
     ) -> Result<Vec<(sig::PublicKey, Signed<Prekey>)>, Error>;
 
-    async fn add_to_group<Users: Stream<Item = UserId>>(
+    async fn add_to_group<Users: Stream<Item = UserId> + Send>(
         &mut self,
         users: Users,
         conv: ConversationId,
     ) -> Result<add_to_group::Res, Error>;
 
-    async fn leave_group<Convs: Stream<Item = ConversationId>>(
+    async fn leave_group<Convs: Stream<Item = ConversationId> + Send>(
         &mut self,
         user: UserId,
         groups: Convs,
@@ -69,7 +69,7 @@ pub trait ServerStore {
         of: sig::PublicKey,
     ) -> Vec<(Push, i64)>;
 
-    async fn del_pending<S: Stream<Item = i64>>(
+    async fn del_pending<S: Stream<Item = i64> + Send>(
         &mut self,
         of: sig::PublicKey,
         items: S,
@@ -79,6 +79,99 @@ pub trait ServerStore {
         &mut self,
         init: Signed<UserId>,
     ) -> Result<register::Res, Error>;
+}
+
+#[async_trait]
+impl ServerStore for Conn {
+    async fn get_sigchain(
+        &mut self,
+        user: UserId,
+    ) -> Result<Option<sig::SigChain>, Error> {
+        unimplemented!()
+    }
+
+    async fn recip_exists(
+        &mut self,
+        user: Recip,
+    ) -> Result<bool, Error> {
+        unimplemented!()
+    }
+
+    async fn add_to_sigchain(
+        &mut self,
+        new: Signed<sig::SigUpdate>,
+    ) -> Result<PKIResponse, Error> {
+        unimplemented!()
+    }
+
+    async fn user_of(
+        &mut self,
+        key: sig::PublicKey,
+    ) -> Result<Option<UserId>, Error> {
+        unimplemented!()
+    }
+
+    async fn new_prekeys<Keys: Stream<Item = (Signed<Prekey>, Option<Prekey>)> + Send>(
+        &mut self,
+        keys: Keys,
+    ) -> Result<new_prekeys::Res, Error> {
+        unimplemented!()
+    }
+
+    async fn get_random_prekeys<Keys: Stream<Item = sig::PublicKey> + Send>(
+        &mut self,
+        keys: Keys,
+    ) -> Result<Vec<(sig::PublicKey, Signed<Prekey>)>, Error> {
+        unimplemented!()
+    }
+
+    async fn add_to_group<Users: Stream<Item = UserId> + Send>(
+        &mut self,
+        users: Users,
+        conv: ConversationId,
+    ) -> Result<add_to_group::Res, Error> {
+        unimplemented!()
+    }
+
+    async fn leave_group<Convs: Stream<Item = ConversationId> + Send>(
+        &mut self,
+        user: UserId,
+        groups: Convs,
+    ) -> Result<leave_groups::Res, Error> {
+        unimplemented!()
+    }
+
+    // should be done transactionally, returns Missing(r) for the first missing recip r
+    // only adds to pending when it finds all devices
+    async fn add_to_pending_and_get_valid_devs(
+        &mut self,
+        recip: &Recip,
+        msg: &Push,
+    ) -> Result<PushedTo, Error> {
+        unimplemented!()
+    }
+
+    async fn get_pending(
+        &mut self,
+        of: sig::PublicKey,
+    ) -> Vec<(Push, i64)> {
+        unimplemented!()
+    }
+
+    async fn del_pending<S: Stream<Item = i64> + Send>(
+        &mut self,
+        of: sig::PublicKey,
+        items: S,
+    ) -> Result<(), Error> {
+        unimplemented!()
+    }
+
+    async fn new_user(
+        &mut self,
+        init: Signed<UserId>,
+    ) -> Result<register::Res, Error> {
+        unimplemented!()
+    }
 }
 
 // macro_rules! sql {
