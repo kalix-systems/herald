bc3460a07d25b688ef02c6c9abbf827161646af3
mobile-bungalow <pwmay@ucsc.edu>
2019-12-20 15:27:19 -0800

    fix undefined errors for ios builds

--- client/foundation/objectiveutils.mm
+++ client/foundation/objectiveutils.mm
@@ -12,8 +12,11 @@ void ObjectiveUtils::set_status_bar_color(QColor color) {
        UIApplication *app =  [UIApplication sharedApplication];
        app.windows.firstObject.rootViewController.view.backgroundColor
            =  [UIColor colorWithRed:color.redF() green:color.greenF() blue:color.blueF()  alpha:1.0];
+
 }
 
+void ObjectiveUtils::send_notification(QString content) {
 
+}
 
 #endif
