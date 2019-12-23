f1fddfb499caf9be7d58018705183b472c436581
theseriousadult <jack@kalix.io>
2019-12-23 15:53:52 -0800

    start rewriting server_protocol

--- herald_server/server_protocol/src/lib.rs
+++ herald_server/server_protocol/src/lib.rs
@@ -13,16 +13,12 @@ use tokio::{
         },
         oneshot,
     },
-    timer::Timeout,
 };
-use warp::filters::ws::{self, WebSocket};
-
-type WTx = SplitSink<WebSocket, ws::Message>;
 
 pub struct ActiveSession {
     interrupt: Trigger,
     done: Tripwire,
-    emitter: Sender<()>,
+    emitter: Sender<Push>,
 }
 
 impl ActiveSession {
@@ -38,8 +34,11 @@ impl ActiveSession {
         done.await
     }
 
-    pub fn emit(&self) -> Result<(), tokio::sync::mpsc::error::UnboundedTrySendError<()>> {
-        self.emitter.clone().try_send(())
+    pub fn push(
+        &self,
+        push: &Push,
+    ) -> Result<(), tokio::sync::mpsc::error::SendError<Push>> {
+        self.emitter.clone().send(push.clone())
     }
 }
 
@@ -51,193 +50,109 @@ pub struct State {
     pub pool: Pool,
 }
 
-// pub mod get;
-// pub mod login;
-// pub mod post;
+impl State {
+    pub fn new() -> Self {
+        State {
+            active: DashMap::default(),
+            pool: Pool::new(),
+        }
+    }
 
-// impl State {
-//     pub fn new() -> Self {
-//         State {
-//             active: DashMap::default(),
-//             pool: Pool::new(),
-//         }
-//     }
+    pub async fn new_connection(&self) -> Result<Conn, Error> {
+        Ok(self.pool.get().await?)
+    }
 
-//     pub async fn new_connection(&self) -> Result<Conn, Error> {
-//         Ok(self.pool.get().await?)
-//     }
+    // pub async fn handle_login(
+    //     &'static self,
+    //     ws: WebSocket,
+    // ) -> Result<(), Error> {
+    //     let mut store: Conn = self.new_connection().await?;
 
-//     pub async fn handle_login(
-//         &'static self,
-//         ws: WebSocket,
-//     ) -> Result<(), Error> {
-//         let mut store: Conn = self.new_connection().await?;
+    //     // all the channels we'll need for plumbing
+    //     // first we split the websocket
+    //     let (mut wtx, mut wrx) = ws.split();
+    //     // bytevec messages received from the socket
+    //     let (mut rtx, mut rrx) = channel::<Vec<u8>>();
+    //     // push emitter which will be stored in the active sessions dashmap
+    //     let (ptx, prx) = channel::<()>();
+    //     // session-close emitter
+    //     let (close, closed) = oneshot::channel::<()>();
 
-//         // all the channels we'll need for plumbing
-//         // first we split the websocket
-//         let (mut wtx, mut wrx) = ws.split();
-//         // bytevec messages received from the socket
-//         let (mut rtx, mut rrx) = channel::<Vec<u8>>();
-//         // push emitter which will be stored in the active sessions dashmap
-//         let (ptx, prx) = channel::<()>();
-//         // session-close emitter
-//         let (close, closed) = oneshot::channel::<()>();
+    //     // on graceful exit, notify runtime to close channel
+    //     // we set things up this way so that the rrx channel
+    //     // will be populated before we call login, hence before
+    //     // we know the gid
+    //     tokio::spawn(async move {
+    //         while let Some(Ok(m)) = wrx.next().await {
+    //             if m.is_close() || (m.is_binary() && rtx.send(m.into_bytes()).await.is_err()) {
+    //                 break;
+    //             }
+    //         }
 
-//         // on graceful exit, notify runtime to close channel
-//         // we set things up this way so that the rrx channel
-//         // will be populated before we call login, hence before
-//         // we know the gid
-//         tokio::spawn(async move {
-//             while let Some(Ok(m)) = wrx.next().await {
-//                 if m.is_close() || (m.is_binary() && rtx.send(m.into_bytes()).await.is_err()) {
-//                     break;
-//                 }
-//             }
+    //         close.send(()).ok();
+    //     });
 
-//             close.send(()).ok();
-//         });
+    //     let gid: GlobalId = login::login(&self.active, &mut store, &mut wtx, &mut rrx).await?;
 
-//         let gid: GlobalId = login::login(&self.active, &mut store, &mut wtx, &mut rrx).await?;
+    //     let (interrupt, prx) = Valved::new(prx);
+    //     let (trigger_done, done) = Tripwire::new();
+    //     let sess = ActiveSession {
+    //         interrupt,
+    //         done,
+    //         emitter: ptx,
+    //     };
+    //     self.active.insert(gid.did, sess);
 
-//         let (interrupt, prx) = Valved::new(prx);
-//         let (trigger_done, done) = Tripwire::new();
-//         let sess = ActiveSession {
-//             interrupt,
-//             done,
-//             emitter: ptx,
-//         };
-//         self.active.insert(gid.did, sess);
+    //     // remove active session on graceful exit
+    //     tokio::spawn(async move {
+    //         drop(closed.await);
+    //         if let Some((_, s)) = self.active.remove(&gid.did) {
+    //             s.interrupt().await;
+    //         }
+    //     });
 
-//         // remove active session on graceful exit
-//         tokio::spawn(async move {
-//             drop(closed.await);
-//             if let Some((_, s)) = self.active.remove(&gid.did) {
-//                 s.interrupt().await;
-//             }
-//         });
+    //     // TODO: handle this error somehow?
+    //     // for now we're just dropping it
+    //     if catchup(gid.did, &mut store, &mut wtx, &mut rrx)
+    //         .await
+    //         .is_ok()
+    //     {
+    //         let mut prx: Timeout<Valved<Receiver<()>>> = prx.timeout(Duration::from_secs(60));
+    //         drop(
+    //             self.send_pushes(&mut store, &mut wtx, &mut rrx, &mut prx, gid.did)
+    //                 .await,
+    //         );
+    //         trigger_done.cancel();
+    //     }
 
-//         // TODO: handle this error somehow?
-//         // for now we're just dropping it
-//         if catchup(gid.did, &mut store, &mut wtx, &mut rrx)
-//             .await
-//             .is_ok()
-//         {
-//             let mut prx: Timeout<Valved<Receiver<()>>> = prx.timeout(Duration::from_secs(60));
-//             drop(
-//                 self.send_pushes(&mut store, &mut wtx, &mut rrx, &mut prx, gid.did)
-//                     .await,
-//             );
-//             trigger_done.cancel();
-//         }
+    //     if let Some((_, s)) = self.active.remove(&gid.did) {
+    //         s.interrupt().await;
+    //     }
 
-//         if let Some((_, s)) = self.active.remove(&gid.did) {
-//             s.interrupt().await;
-//         }
+    //     Ok(())
+    // }
 
-//         Ok(())
-//     }
+    // async fn send_pushes(
+    //     &self,
+    //     store: &mut Conn,
+    //     wtx: &mut WTx,
+    //     rrx: &mut Receiver<Vec<u8>>,
+    //     rx: &mut Timeout<Valved<Receiver<()>>>,
+    //     did: sig::PublicKey,
+    // ) -> Result<(), Error> {
+    //     while let Some(p) = rx.next().await {
+    //         if p.is_ok() {
+    //             catchup(did, store, wtx, rrx).await?;
+    //         } else {
+    //             wtx.send(ws::Message::ping(vec![0u8]))
+    //                 .timeout(Duration::from_secs(5))
+    //                 .await??;
+    //         }
+    //     }
 
-//     pub async fn push_users(
-//         &self,
-//         req: push_users::Req,
-//     ) -> Result<push_users::Res, Error> {
-//         let push_users::Req { to, exc, msg } = req;
-//         let msg: Push = Push {
-//             tag: PushTag::User,
-//             timestamp: Time::now(),
-//             msg,
-//         };
-
-//         let mut missing_users: Vec<UserId> = Vec::new();
-//         let mut to_devs: Vec<sig::PublicKey> = Vec::new();
-//         let mut conn: Conn = self.new_connection().await?;
-
-//         for user in to {
-//             if !conn.user_exists(&user).await? {
-//                 missing_users.push(user);
-//             } else {
-//                 for dev in conn.valid_keys(&user).await? {
-//                     if dev != exc {
-//                         to_devs.push(dev);
-//                     }
-//                 }
-//             }
-//         }
-
-//         Ok(if !missing_users.is_empty() {
-//             push_users::Res::Missing(missing_users)
-//         } else {
-//             self.send_push_to_devices(&mut conn, to_devs, msg).await?;
-//             push_users::Res::Success
-//         })
-//     }
-
-//     pub async fn push_devices(
-//         &self,
-//         req: push_devices::Req,
-//     ) -> Result<push_devices::Res, Error> {
-//         let push_devices::Req { to, msg } = req;
-//         let msg = Push {
-//             tag: PushTag::Device,
-//             timestamp: Time::now(),
-//             msg,
-//         };
-
-//         let mut conn = self.new_connection().await?;
-//         let mut missing_devs: Vec<sig::PublicKey> = Vec::new();
-
-//         for dev in to.iter() {
-//             if !conn.device_exists(dev).await? {
-//                 missing_devs.push(*dev);
-//             }
-//         }
-
-//         Ok(if !missing_devs.is_empty() {
-//             push_devices::Res::Missing(missing_devs)
-//         } else {
-//             self.send_push_to_devices(&mut conn, to, msg).await?;
-//             push_devices::Res::Success
-//         })
-//     }
-
-//     async fn send_push_to_devices(
-//         &self,
-//         con: &mut Conn,
-//         to_devs: Vec<sig::PublicKey>,
-//         msg: Push,
-//     ) -> Result<(), Error> {
-//         con.add_pending(to_devs.clone(), &[msg]).await?;
-
-//         for dev in to_devs {
-//             if let Some(s) = self.active.async_get(dev).await {
-//                 drop(s.emit());
-//             }
-//         }
-
-//         Ok(())
-//     }
-
-//     async fn send_pushes(
-//         &self,
-//         store: &mut Conn,
-//         wtx: &mut WTx,
-//         rrx: &mut Receiver<Vec<u8>>,
-//         rx: &mut Timeout<Valved<Receiver<()>>>,
-//         did: sig::PublicKey,
-//     ) -> Result<(), Error> {
-//         while let Some(p) = rx.next().await {
-//             if p.is_ok() {
-//                 catchup(did, store, wtx, rrx).await?;
-//             } else {
-//                 wtx.send(ws::Message::ping(vec![0u8]))
-//                     .timeout(Duration::from_secs(5))
-//                     .await??;
-//             }
-//         }
-
-//         Ok(())
-//     }
-// }
+    //     Ok(())
+    // }
+}
 
 // async fn catchup(
 //     did: sig::PublicKey,
