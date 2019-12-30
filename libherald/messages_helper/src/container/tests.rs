7948a020f804cf3aa531e23eab3dac9a2edb9c3e
Kaavya <kaavya@berkeley.edu>
2019-12-29 18:35:52 -0800

    container test setup

--- libherald/messages_helper/src/container/tests.rs
+++ libherald/messages_helper/src/container/tests.rs
@@ -1,8 +1,8 @@
 use super::*;
 use coremacros::*;
+use message_cache::cache;
 use std::collections::{HashMap, HashSet};
 use std::convert::TryInto;
-use types::*;
 
 fn msg_constructor(
     id: MsgId,
@@ -36,7 +36,20 @@ fn msg_constructor(
 #[test]
 
 fn test_container() {
+    let msgid1 = [0; 32].into();
+    let msgid2 = [1; 32].into();
     let author = "author".try_into().expect(womp!());
-    let msg1 = msg_constructor([0; 32].into(), author);
-    let msg2 = msg_constructor([1; 32].into(), author);
+    let msg1 = msg_constructor(msgid1, author);
+    let msg2 = msg_constructor(msgid2, author);
+
+    cache();
+
+    let (msgmeta1, msgdata1) = coretypes::messages::split_msg(msg1);
+    let (msgmeta2, msgdata2) = coretypes::messages::split_msg(msg2);
+    let container = Container::new(vec![msgmeta1, msgmeta2], Some(msgdata2.clone()));
+
+    cache::insert(msgid1, msgdata1);
+    cache::insert(msgid2, msgdata2);
+
+    assert_eq!(container.len(), 2);
 }
