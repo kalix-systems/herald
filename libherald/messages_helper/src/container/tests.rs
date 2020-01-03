df681a434acd4d6f45c70fddb6a2bb99cdcd8274
Kaavya <kaavya@berkeley.edu>
2020-01-02 20:43:09 -0800

    tests pass

--- libherald/messages_helper/src/container/tests.rs
+++ libherald/messages_helper/src/container/tests.rs
@@ -1,7 +1,64 @@
 use super::*;
 use coremacros::*;
+use parking_lot::Mutex;
 use serial_test_derive::serial;
 use std::convert::TryInto;
+
+pub struct TestModel {
+    data_changed_state: Vec<(usize, usize)>,
+}
+
+impl TestModel {
+    fn new() -> TestModel {
+        TestModel {
+            data_changed_state: Vec::new(),
+        }
+    }
+}
+
+impl MessageModel for TestModel {
+    fn data_changed(
+        &mut self,
+        a: usize,
+        b: usize,
+    ) {
+        self.data_changed_state.push((a, b));
+    }
+}
+
+pub struct TestEmit {
+    num_matches_state: u64,
+    pattern_changed_state: u64,
+    regex_changed_state: u64,
+    index_changed_state: u64,
+}
+
+impl TestEmit {
+    fn new() -> TestEmit {
+        TestEmit {
+            num_matches_state: 0,
+            pattern_changed_state: 0,
+            regex_changed_state: 0,
+            index_changed_state: 0,
+        }
+    }
+}
+
+impl MessageEmit for TestEmit {
+    fn search_index_changed(&mut self) {
+        self.index_changed_state += 1;
+    }
+    fn search_num_matches_changed(&mut self) {
+        self.num_matches_state += 1;
+    }
+
+    fn search_pattern_changed(&mut self) {
+        self.pattern_changed_state += 1;
+    }
+    fn search_regex_changed(&mut self) {
+        self.regex_changed_state += 1;
+    }
+}
 
 fn msg_constructor(body: &str) -> (MessageMeta, MsgData) {
     let convid = [0; 32].into();
@@ -82,16 +139,26 @@ fn test_container_search() {
 
     let mut searchstate = SearchState::new();
 
+    let emit = &mut TestEmit::new();
+    let model = &mut TestModel::new();
     searchstate
-        .set_pattern("te".to_string(), || ())
+        .set_pattern("te".to_string(), emit)
         .expect(womp!());
     searchstate.active = true;
 
+    assert_eq!(emit.pattern_changed_state, 1);
+
     let matches = container
-        .apply_search(&searchstate, |_| (), || ())
+        .apply_search(&searchstate, emit, model)
         .expect(womp!("Failed to apply search"));
 
-    searchstate.set_matches(matches, || (), || ());
+    assert_eq!(emit.num_matches_state, 1);
+
+    searchstate.set_matches(matches, emit);
+
+    assert_eq!(emit.num_matches_state, 2);
+
+    assert_eq!(emit.index_changed_state, 1);
 
     assert_eq!(searchstate.num_matches(), 2);
 
@@ -114,13 +181,16 @@ fn test_container_search() {
         container.get(1).expect(womp!())
     );
 
-    container.clear_search(|_| ());
+    container.clear_search(model);
 
     assert_eq!(msgmeta1.match_status, MatchStatus::NotMatched);
 
     assert_eq!(msgmeta2.match_status, MatchStatus::NotMatched);
 
     assert_eq!(msgmeta3.match_status, MatchStatus::NotMatched);
+
+    assert_eq!(model.data_changed_state[0], (1, 1));
+    assert_eq!(model.data_changed_state[1], (2, 2));
 }
 
 #[test]
