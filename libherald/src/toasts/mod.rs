327f424e9136a018109806ecf20f548bb38f837e
Baeo Maltinsky <baeo.maltinsky@gmail.com>
2020-01-05 19:56:52 -0800

    Remove drop

--- libherald/src/toasts/mod.rs
+++ libherald/src/toasts/mod.rs
@@ -50,7 +50,7 @@ mod imp {
         *IS_SET
             .get_or_try_init(|| {
                 let bundle = get_bundle_identifier_or_default(super::DESKTOP_APP_NAME);
-                drop(set_application(&bundle));
+                set_application(&bundle)
             })
             .unwrap_or(&false)
     }
