de4722a42e95ceab0b70ca408f6ab5c0aed8a687
Baeo Maltinsky <baeo.maltinsky@gmail.com>
2020-01-08 18:02:27 -0800

    Bring back more message

--- heraldcore/src/message/sql/conversation_message_meta.sql
+++ heraldcore/src/message/sql/conversation_message_meta.sql
@@ -5,6 +5,6 @@ FROM
   messages
 WHERE
   messages.conversation_id = @conversation_id AND
-  (messages.expiration_ts IS NULL OR messages.expiration_ts < @current_time)
+  (messages.expiration_ts IS NULL OR messages.expiration_ts > @current_time)
 ORDER BY
   insertion_ts ASC
