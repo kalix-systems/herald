4e34f3cbce33c391ebc97dc3c7c34ffdaa6ad873
Baeo Maltinsky <baeo.maltinsky@gmail.com>
2020-01-08 22:09:03 -0800

    order shared by last active time

--- heraldcore/src/members/sql/shared_conversations.sql
+++ heraldcore/src/members/sql/shared_conversations.sql
@@ -1,8 +1,13 @@
 SELECT
-    conversation_id
+    conversation_members.conversation_id
 FROM
     conversation_members
+INNER JOIN
+    conversations
+ON
+    conversations.conversation_id = conversation_members.conversation_id
 WHERE
-    member_id = @user_id
-AND
-    conversation_id != @pairwise_cid
+    member_id = @user_id AND
+    conversation_members.conversation_id != @pairwise_cid
+ORDER BY
+    conversations.last_active_ts DESC
