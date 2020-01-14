9e01fcdadb71a3eb63d2d15ba79732cb58ff7b44
Baeo Maltinsky <baeo.maltinsky@gmail.com>
2020-01-14 13:33:34 -0800

    Add sync kson_channel test

--- kson_channel/tests/tcp_sync.rs
+++ kson_channel/tests/tcp_sync.rs
@@ -1,8 +1,7 @@
-#![cfg(feature = "async")]
-
 use kson::prelude::*;
 use kson_channel::*;
-use tokio::net::*;
+use std::net::{TcpListener, TcpStream};
+use std::thread::spawn;
 
 const NUM_STREAMS: usize = 100;
 const NUM_ITERS: usize = 10;
@@ -11,66 +10,55 @@ const PACKET_SIZE: usize = 1024;
 
 const MSG: &'static [u8] = &[1; PACKET_SIZE * 10];
 
-#[tokio::test]
-async fn echo() {
+#[test]
+fn echo() {
     let addr: (&str, u16) = (
         "127.0.0.1",
         port_check::free_local_port().expect("failed to find free port"),
     );
 
-    let server = tokio::spawn(async move {
+    let server = spawn(move || {
         let mut joins = Vec::with_capacity(NUM_STREAMS);
 
-        let mut listener = TcpListener::bind(addr.clone())
-            .await
-            .expect("failed to bind to free port");
+        let listener = TcpListener::bind(addr.clone()).expect("failed to bind to free port");
 
         for _ in 0..NUM_STREAMS {
-            let (stream, _) = listener
-                .accept()
-                .await
-                .expect("failed to get next tcp stream");
+            let (stream, _) = listener.accept().expect("failed to get next tcp stream");
 
-            joins.push(tokio::spawn(async move {
+            joins.push(spawn(move || {
                 let mut framed = Framed::new(stream);
 
                 for i in 0..NUM_ITERS {
                     let msg: Bytes = framed
-                        .read_de()
-                        .await
+                        .read_de_sync()
                         .expect(&format!("Failed to read ping {}", i));
                     assert_eq!(msg, Bytes::from_static(MSG));
                     framed
-                        .write_ser(&Bytes::from_static(MSG))
-                        .await
+                        .write_ser_sync(&Bytes::from_static(MSG))
                         .expect(&format!("Failed to write pong {}", i));
                 }
             }));
         }
 
         for f in joins {
-            f.await.expect("a server task panicked");
+            f.join().expect("a server task panicked");
         }
     });
 
-    let clients = tokio::spawn(async move {
+    let clients = spawn(move || {
         let mut joins = Vec::with_capacity(NUM_STREAMS);
 
         for _ in 0..NUM_STREAMS {
-            joins.push(tokio::spawn(async move {
-                let stream = TcpStream::connect(addr.clone())
-                    .await
-                    .expect("failed to connect to server");
+            joins.push(spawn(move || {
+                let stream = TcpStream::connect(addr.clone()).expect("failed to connect to server");
 
                 let mut framed = Framed::new(stream);
                 for i in 0..NUM_ITERS {
                     framed
-                        .write_ser(&Bytes::from_static(MSG))
-                        .await
+                        .write_ser_sync(&Bytes::from_static(MSG))
                         .expect(&format!("Failed to write ping {}", i));
                     let msg: Bytes = framed
-                        .read_de()
-                        .await
+                        .read_de_sync()
                         .expect(&format!("Failed to write pong {}", i));
                     assert_eq!(msg, Bytes::from_static(MSG));
                 }
@@ -78,10 +66,10 @@ async fn echo() {
         }
 
         for f in joins {
-            f.await.expect("a server task panicked");
+            f.join().expect("a server task panicked");
         }
     });
 
-    server.await.expect("server panicked");
-    clients.await.expect("client panicked");
+    server.join().expect("server panicked");
+    clients.join().expect("client panicked");
 }
