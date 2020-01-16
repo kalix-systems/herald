b20a336fcfaa52728a9038de24165a85f798d445
theseriousadult <jack@kalix.io>
2020-01-15 19:36:48 -0800

    add stuff re generating tls pairs to server

--- herald_server/src/lib.rs
+++ herald_server/src/lib.rs
@@ -1,45 +1,61 @@
-use bytes::Buf;
-use herald_common::*;
-use server_errors::*;
-use server_protocol::*;
-use server_store::*;
-use std::future::Future;
+use anyhow::Error;
+use rustls::*;
+use std::{fs, path::Path, sync::Arc};
+use tokio_rustls::*;
 
-async fn req_handler_store<B, I, O, F, Fut>(
-    state: &State,
-    req: B,
-    f: F,
-) -> Result<Vec<u8>, Error>
-where
-    B: Buf,
-    I: De,
-    O: Ser,
-    F: FnOnce(Conn, I) -> Fut,
-    Fut: Future<Output = Result<O, Error>>,
-{
-    let con: Conn = state.new_connection().await?;
-    let buf: Bytes = req.collect();
-    let req: I = kson::from_bytes(buf)?;
-    let res: O = f(con, req).await?;
-    let res_ser: Vec<u8> = kson::to_vec(&res);
-    Ok(res_ser)
+pub struct TlsKeyPair {
+    pub cert_der: Vec<u8>,
+    pub priv_key: Vec<u8>,
 }
 
-async fn req_handler_async<'a, B, I, O, F, Fut>(
-    state: &'a State,
-    req: B,
-    f: F,
-) -> Result<Vec<u8>, Error>
-where
-    B: Buf,
-    I: De,
-    O: Ser,
-    F: FnOnce(&'a State, I) -> Fut,
-    Fut: Future<Output = Result<O, Error>>,
-{
-    let buf: Bytes = req.collect();
-    let req: I = kson::from_bytes(buf)?;
-    let res: O = f(state, req).await?;
-    let res_ser: Vec<u8> = kson::to_vec(&res);
-    Ok(res_ser)
+impl TlsKeyPair {
+    pub fn gen_new_self_signed<H: Into<Vec<String>>>(hosts: H) -> Result<Self, Error> {
+        let cert = rcgen::generate_simple_self_signed(hosts)?;
+        let cert_der = cert.serialize_der()?;
+        let priv_key = cert.serialize_private_key_der();
+        Ok(TlsKeyPair { cert_der, priv_key })
+    }
+
+    pub fn write_to_dir<P: AsRef<Path>>(
+        &self,
+        dir: &P,
+    ) -> Result<(), std::io::Error> {
+        let mut pb = dir.as_ref().to_path_buf();
+        {
+            pb.push("cert");
+            fs::write(&pb, &self.cert_der)?;
+            pb.pop();
+        }
+        {
+            pb.push("priv");
+            fs::write(&pb, &self.priv_key)?;
+            pb.pop();
+        }
+        Ok(())
+    }
+
+    pub fn read_from_dir<P: AsRef<Path>>(dir: &P) -> Result<Self, std::io::Error> {
+        let mut pb = dir.as_ref().to_path_buf();
+        let cert_der;
+        let priv_key;
+        {
+            pb.push("cert");
+            cert_der = fs::read(&pb)?;
+            pb.pop();
+        }
+        {
+            pb.push("priv");
+            priv_key = fs::read(&pb)?;
+            pb.pop();
+        }
+        Ok(TlsKeyPair { cert_der, priv_key })
+    }
+
+    pub fn configure_server(&self) -> Result<TlsAcceptor, Error> {
+        let mut config = rustls::ServerConfig::new(Arc::new(NoClientAuth));
+        let pk = PrivateKey(self.priv_key.clone());
+        config.set_single_cert(vec![Certificate(self.cert_der.clone())], pk)?;
+
+        Ok(Arc::new(config).into())
+    }
 }
