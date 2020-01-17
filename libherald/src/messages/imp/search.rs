2872468a0226dc49d8abcd9d8411dfb126ced0c5
Kaavya <kaavya@berkeley.edu>
2020-01-16 20:07:04 -0800

    fix reverse search index

--- libherald/src/messages/imp/search.rs
+++ libherald/src/messages/imp/search.rs
@@ -140,9 +140,6 @@ impl Messages {
     }
 
     pub(crate) fn search_index_(&self) -> u64 {
-        self.search
-            .index
-            .map(|ix| self.search.num_matches().saturating_sub(ix))
-            .unwrap_or(0) as u64
+        self.search.index.map(|ix| ix + 1).unwrap_or(0) as u64
     }
 }
