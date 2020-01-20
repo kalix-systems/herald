2ceca61a3e3fb44753a436fcb280b9cc5b14bb08
mobile-bungalow <pwmay@ucsc.edu>
2020-01-20 15:10:36 -0800

    used proper scope to get push and notification

--- libherald/src/toasts/mod.rs
+++ libherald/src/toasts/mod.rs
@@ -83,7 +83,7 @@ mod imp {
 
 #[cfg(target_os = "windows")]
 mod imp {
-    use crate::herald::shared::{push, Update};
+    use crate::herald::{push, Update};
     use heraldcore::message::Message;
 
     pub fn new_msg_toast(msg: &Message) {
