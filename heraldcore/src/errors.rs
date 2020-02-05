d82ad5f679e28ea75ec5613f59e57f66d5e24cfc
theseriousadult <jack@kalix.io>
2020-02-05 14:15:48 -0800

    delete error boilerplate

--- heraldcore/src/errors.rs
+++ heraldcore/src/errors.rs
@@ -69,69 +69,6 @@ pub enum HErr {
     BadSocketAddr(#[from] std::net::AddrParseError),
 }
 
-// impl fmt::Display for HErr {
-//     fn fmt(
-//         &self,
-//         f: &mut fmt::Formatter<'_>,
-//     ) -> fmt::Result {
-//         use HErr::*;
-//         match self {
-//             DatabaseError(e) => write!(f, "Database Error: {}", e),
-//             HeraldError(s) => write!(f, "Herald Error: {}", s),
-//             IoError(e) => write!(f, "IoError: {}", e),
-//             ImageError(s) => write!(f, "ImageError: {}", s),
-//             KsonError(e) => write!(f, "KsonError error: {}", e),
-//             BadPath(s) => write!(f, "Bad path: {:?}", s),
-//             RegexError(e) => write!(f, "RegexError: {}", e),
-//             GIDSpecFailed(lt) => write!(f, "GIDSpecFailed: {:?}", lt),
-//             SignInFailed(lt) => write!(f, "SignInFailed: {:?}", lt),
-//             WebsocketError(e) => write!(f, "WebsocketError: {}", e),
-//             MissingOutboundMessageField(missing) => write!(f, "{}", missing),
-//             MissingInboundMessageField(missing) => write!(f, "{}", missing),
-//             NoneError(location) => write!(f, "Unexpected none at {}", location),
-//             ChannelSendError(location) => write!(f, "Channel send error at {}", location),
-//             ChannelRecvError(location) => write!(f, "Channel receive error at {}", location),
-//             BadRandomId(e) => write!(f, "{}", e),
-//             Attachment(e) => write!(f, "{}", e),
-//             EmptyMessageBody(e) => write!(f, "{}", e),
-//             BadSocketAddr(e) => write!(f, "{}", e),
-//         }
-//     }
-// }
-
-// impl std::error::Error for HErr {
-//     fn cause(&self) -> Option<&(dyn std::error::Error + 'static)> {
-//         use HErr::*;
-//         Some(match self {
-//             DatabaseError(e) => e,
-//             IoError(e) => e,
-//             ImageError(s) => s,
-//             // KsonError(e) => e,
-//             RegexError(e) => e,
-//             WebsocketError(e) => e,
-//             _ => return None,
-//         })
-//     }
-// }
-
-// macro_rules! herr {
-//     ($from:ty, $fn:ident) => {
-//         from_fn!(HErr, $from, HErr::$fn);
-//     };
-// }
-
-// herr!(MissingOutboundMessageField, MissingOutboundMessageField);
-// herr!(MissingInboundMessageField, MissingInboundMessageField);
-// herr!(EmptyMessageBody, EmptyMessageBody);
-// herr!(rusqlite::Error, DatabaseError);
-// herr!(std::io::Error, IoError);
-// herr!(KsonError, KsonError);
-// herr!(websocket::result::WebSocketError, WebsocketError);
-// herr!(search_pattern::SearchPatternError, RegexError);
-// herr!(std::ffi::OsString, BadPath);
-// herr!(herald_attachments::Error, Attachment);
-// herr!(std::net::AddrParseError, BadSocketAddr);
-
 impl From<image_utils::ImageError> for HErr {
     fn from(e: image_utils::ImageError) -> Self {
         use image_utils::ImageError;
