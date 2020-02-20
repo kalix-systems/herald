b4107de3f9246dd13ab1ed578b819444b516e641
theseriousadult <jack@kalix.io>
2020-02-19 22:28:34 -0800

    network functions uncommented

--- heraldcore/src/network/mod.rs
+++ heraldcore/src/network/mod.rs
@@ -58,8 +58,12 @@ pub fn dep_key(to_dep: sig::PublicKey) -> Result<PKIResponse, HErr> {
 
     if res == PKIResponse::Success {
         store.extend_sigchain(gid.uid, update.clone());
+
         let as_msg = proto::Msg::SigUpdate(update);
         let users: Vec<UserId> = w!(store.get_all_users());
+
+        w!(store.commit());
+
         w!(helper::push(&push::Req {
             from: gid,
             to: Recip::Many(Recips::Users(users)),
@@ -67,17 +71,36 @@ pub fn dep_key(to_dep: sig::PublicKey) -> Result<PKIResponse, HErr> {
         }));
     }
 
-    w!(store.commit());
-
     Ok(res)
 }
 
-// /// Adds new key to the server's key registry.
-// pub fn new_key(to_new: sig::PublicKey) -> Result<PKIResponse, HErr> {
-//     let kp = w!(config::keypair());
-//     let req = new_key::Req(sign_ser(&kp, to_new));
-//     Ok(w!(helper::new_key(&req)).0)
-// }
+/// Adds new key to the server's key registry.
+pub fn new_key(to_new: Signed<UserId>) -> Result<PKIResponse, HErr> {
+    get_crypto_conn!(store);
+
+    let kp = w!(config::keypair());
+    let gid = w!(config::gid());
+    let update = sign_ser(&kp, sig::SigUpdate::Endorse(to_new));
+
+    let res = w!(helper::new_sig(&Box::new(update)));
+
+    if res == PKIResponse::Success {
+        store.extend_sigchain(gid.uid, update.clone());
+
+        let as_msg = proto::Msg::SigUpdate(update);
+        let users: Vec<UserId> = w!(store.get_all_users());
+
+        w!(store.commit());
+
+        w!(helper::push(&push::Req {
+            from: gid,
+            to: Recip::Many(Recips::Users(users)),
+            msg: kson::to_vec(&as_msg).into(),
+        }));
+    }
+
+    Ok(res)
+}
 
 /// Registers new user on the server.
 pub fn register(
@@ -109,30 +132,30 @@ pub fn register(
     Ok(res)
 }
 
-// /// Sends a message read receipt
-// pub fn send_read_receipt(
-//     cid: ConversationId,
-//     msg_id: MsgId,
-// ) -> Result<(), HErr> {
-//     w!(send_cmessage(
-//         cid,
-//         &ConversationMessage::Message(NetContent::Receipt(cmessages::Receipt {
-//             of: msg_id,
-//             stat: ReceiptStatus::Read,
-//         })),
-//     ));
-//     Ok(())
-// }
+/// Sends a message read receipt
+pub fn send_read_receipt(
+    cid: ConversationId,
+    msg_id: MsgId,
+) -> Result<(), HErr> {
+    w!(send_cmessage(
+        cid,
+        ConversationMessage::Message(NetContent::Receipt(cmessages::Receipt {
+            of: msg_id,
+            stat: ReceiptStatus::Read,
+        })),
+    ));
+    Ok(())
+}
 
-// /// Sends a typing indicator
-// pub fn send_typing_indicator(cid: ConversationId) -> Result<(), HErr> {
-//     w!(send_cmessage(
-//         cid,
-//         &ConversationMessage::Message(NetContent::Typing(Time::now())),
-//     ));
+/// Sends a typing indicator
+pub fn send_typing_indicator(cid: ConversationId) -> Result<(), HErr> {
+    w!(send_cmessage(
+        cid,
+        ConversationMessage::Message(NetContent::Typing(Time::now())),
+    ));
 
-//     Ok(())
-// }
+    Ok(())
+}
 
 // /// Sends a user request to `uid` with a proposed conversation id `cid`.
 // pub fn send_user_req(
@@ -147,128 +170,111 @@ pub fn register(
 //     send_umessage(uid, &DeviceMessageBody::Req(req))
 // }
 
-// pub(crate) fn send_normal_message(
-//     cid: ConversationId,
-//     msg: cmessages::Msg,
-// ) -> Result<SendOutcome, HErr> {
-//     let mid = msg.mid;
-//     let outcome = w!(send_cmessage(
-//         cid,
-//         &ConversationMessage::Message(NetContent::Msg(msg))
-//     ));
+pub(crate) fn send_normal_message(
+    cid: ConversationId,
+    msg: cmessages::Msg,
+) -> Result<SendOutcome, HErr> {
+    let mid = msg.mid;
+    let outcome = w!(send_cmessage(
+        cid,
+        ConversationMessage::Message(NetContent::Msg(msg))
+    ));
 
-//     if let SendOutcome::Success = outcome {
-//         {
-//             let conn = w!(crate::db::Database::get());
-//             w!(crate::message::db::update_send_status(
-//                 &conn,
-//                 mid,
-//                 coretypes::messages::SendStatus::Ack,
-//             ));
-//         }
-//     }
+    if let SendOutcome::Success = outcome {
+        {
+            let conn = w!(crate::db::Database::get());
+            w!(crate::message::db::update_send_status(
+                &conn,
+                mid,
+                coretypes::messages::SendStatus::Ack,
+            ));
+        }
+    }
 
-//     Ok(outcome)
-// }
+    Ok(outcome)
+}
 
-// pub(crate) fn send_group_settings_message(
-//     mid: MsgId,
-//     cid: ConversationId,
-//     expiration: Option<Time>,
-//     update: cmessages::GroupSettingsUpdate,
-// ) -> Result<(), HErr> {
-//     if let SendOutcome::Success = w!(send_normal_message(
-//         cid,
-//         cmessages::Msg {
-//             mid,
-//             expiration,
-//             content: cmessages::MsgContent::GroupSettings(update),
-//         },
-//     )) {
-//         crate::push(crate::message::OutboundAux::SendDone(cid, mid));
-//     }
-//     Ok(())
-// }
+pub(crate) fn send_group_settings_message(
+    mid: MsgId,
+    cid: ConversationId,
+    expiration: Option<Time>,
+    update: cmessages::GroupSettingsUpdate,
+) -> Result<(), HErr> {
+    if let SendOutcome::Success = w!(send_normal_message(
+        cid,
+        cmessages::Msg {
+            mid,
+            expiration,
+            content: cmessages::MsgContent::GroupSettings(update),
+        },
+    )) {
+        crate::push(crate::message::OutboundAux::SendDone(cid, mid));
+    }
+    Ok(())
+}
 
-// pub(crate) fn send_profile_update(update: cmessages::ProfileChanged) -> Result<(), HErr> {
-//     let conn = w!(crate::db::Database::get());
+// TODO: send this to all users instead
+pub(crate) fn send_profile_update(update: cmessages::ProfileChanged) -> Result<(), HErr> {
+    let conn = w!(crate::db::Database::get());
 
-//     use cmessages::ProfileChanged as P;
-//     match update {
-//         color @ P::Color(_) => {
-//             let cid = w!(crate::config::db::nts_conversation(&conn));
-//             let msg = ConversationMessage::Message(NetContent::ProfileChanged(color));
+    use cmessages::ProfileChanged as P;
+    match update {
+        color @ P::Color(_) => {
+            let cid = w!(crate::config::db::nts_conversation(&conn));
+            let msg = ConversationMessage::Message(NetContent::ProfileChanged(color));
 
-//             w!(send_cmessage(cid, &msg));
-//         }
-//         other => {
-//             let cids = w!(crate::conversation::db::get_all_pairwise_conversations(
-//                 &conn
-//             ));
+            w!(send_cmessage(cid, msg.clone()));
+        }
+        other => {
+            let cids = w!(crate::conversation::db::get_all_pairwise_conversations(
+                &conn
+            ));
 
-//             let msg = ConversationMessage::Message(NetContent::ProfileChanged(other));
+            let msg = ConversationMessage::Message(NetContent::ProfileChanged(other));
 
-//             for cid in cids {
-//                 w!(send_cmessage(cid, &msg));
-//             }
-//         }
-//     }
+            for cid in cids {
+                w!(send_cmessage(cid, msg.clone()));
+            }
+        }
+    }
 
-//     Ok(())
-// }
+    Ok(())
+}
 
-// /// Sends a reaction
-// pub fn send_reaction(
-//     cid: ConversationId,
-//     msg_id: MsgId,
-//     react_content: crate::message::ReactContent,
-// ) -> Result<(), HErr> {
-//     w!(send_cmessage(
-//         cid,
-//         &ConversationMessage::Message(NetContent::Reaction(cmessages::Reaction {
-//             msg_id,
-//             react_content,
-//             remove: false,
-//         })),
-//     ));
-//     Ok(())
-// }
+/// Sends a reaction
+pub fn send_reaction(
+    cid: ConversationId,
+    msg_id: MsgId,
+    react_content: crate::message::ReactContent,
+) -> Result<(), HErr> {
+    w!(send_cmessage(
+        cid,
+        ConversationMessage::Message(NetContent::Reaction(cmessages::Reaction {
+            msg_id,
+            react_content,
+            remove: false,
+        })),
+    ));
+    Ok(())
+}
 
-// /// Sends a reaction removal update
-// pub fn send_reaction_removal(
-//     cid: ConversationId,
-//     msg_id: MsgId,
-//     react_content: crate::message::ReactContent,
-// ) -> Result<(), HErr> {
-//     w!(send_cmessage(
-//         cid,
-//         &ConversationMessage::Message(NetContent::Reaction(cmessages::Reaction {
-//             msg_id,
-//             react_content,
-//             remove: true,
-//         })),
-//     ));
-//     Ok(())
-// }
+/// Sends a reaction removal update
+pub fn send_reaction_removal(
+    cid: ConversationId,
+    msg_id: MsgId,
+    react_content: crate::message::ReactContent,
+) -> Result<(), HErr> {
+    w!(send_cmessage(
+        cid,
+        ConversationMessage::Message(NetContent::Reaction(cmessages::Reaction {
+            msg_id,
+            react_content,
+            remove: true,
+        })),
+    ));
+    Ok(())
+}
 
 pub(crate) fn server_url(ext: &str) -> String {
     format!("http://{}/{}", home_server(), ext)
 }
-
-// macro_rules! get_of_helper {
-//     ($name: tt, $of: ty, $to: ty) => {
-//         #[allow(missing_docs)]
-//         pub fn $name(of: $of) -> Result<$to, HErr> {
-//             Ok(w!(helper::$name(&$name::Req(of))).0)
-//         }
-//     };
-// }
-
-// get_of_helper!(keys_of, Vec<UserId>, Vec<(UserId, UserMeta)>);
-// get_of_helper!(
-//     key_info,
-//     Vec<sig::PublicKey>,
-//     Vec<(sig::PublicKey, sig::PKMeta)>
-// );
-// get_of_helper!(keys_exist, Vec<sig::PublicKey>, Vec<bool>);
-// get_of_helper!(users_exist, Vec<UserId>, Vec<bool>);
